d9deac0c4921439cd297369082dbd47900a2abed
Airon Tark <airon@tark.pro>
2025-01-19 09:05:40 +0200

    PEEP-6: docker_build_push.sh: add login before push

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,5 +1,6 @@
 
 # Use "--progress=plain --no-cache" to debug
+aws ecr get-login-password --region us-east-2 --profile airon+1@tark.pro | docker login --username AWS --password-stdin 273135773297.dkr.ecr.us-east-2.amazonaws.com
 docker build --platform linux/amd64 -t sneakerx-parser-image .
 docker tag sneakerx-parser-image:latest 273135773297.dkr.ecr.us-east-2.amazonaws.com/sneakerx-repo:latest
 docker push 273135773297.dkr.ecr.us-east-2.amazonaws.com/sneakerx-repo:latest
